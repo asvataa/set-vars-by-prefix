b29020c9c605e4bb5afd22206c4d32c577ae5557
Тимур Наурузбаев <asvata@MacBook-Pro-Timur-2.local>
2023-04-06 15:16:30 +0600

    asvata's changes

--- index.js
+++ index.js
@@ -1,52 +1,24 @@
 const core = require('@actions/core');
 
-try {
-  const multiLineVars = core.getMultilineInput('variables', { required: true });
-  const varsLength = multiLineVars.length;
-
-  if (varsLength > 10) core.setFailed(`Maximum number of variables allowed is 10. You're trying to set ${varsLength} variables`);
-
-  const allowedDataTypes = ['string', 'boolean', 'number'];
-
-  const validateDataType = (item) => {
-    const type = eval('typeof ' + item);
-
-    if (!allowedDataTypes.includes(type)) {
-      console.log(`${item} dataType is not allowed`);
-      return core.setFailed(`only <${allowedDataTypes.join(', ')}> dataTypes are allowed`);
-    };
-  }
-
-  const variables = {};
-
-  for (let i = 0; i < varsLength; i++) {
-    if (multiLineVars[i].indexOf(':') === -1) core.setFailed(`Variable at position ${i+1} should be a key-value pair`);
-
-    const splitVariable = multiLineVars[i].split(':');
-    const key = splitVariable[0];
-    const valueString = multiLineVars[i].replace(`${key}:`, '').trim();
-    const splitValue = valueString.split(' ? ');
-
-    if (splitValue.length > 1) {
-      const operator = splitValue[0].match(/<[=>]?|==|>=?|!=|\&\&|\|\|/g)[0];
-      const splitCondition = splitValue[0].split(operator).map(v => v.trim());
-      const splitResult = splitValue[1].split(' : ').map(v => v.trim());
-      const data = splitCondition.concat(splitResult);
-      const dataLength = data.length;
-
-      for (let j = 0; j < dataLength; j++) {
-        validateDataType(data[j]);
-      }
-
-      variables[key] = eval(valueString);
-    } else {
-      validateDataType(splitValue[0]);
-      variables[key] = eval(splitValue[0]);
+function getVars(vars) {
+  if (typeof vars === "string") {
+    try {
+      return JSON.parse(vars);
+    } catch (err) {
+      return vars;
     }
   }
+  return vars;
+}
 
-  for (let i in variables) {
-    core.exportVariable(i, variables[i]);
+try {
+  const variables = getVars(core.getInput("variables", { required: true }));
+  const values_prefix = core.getInput("varsPrefix");
+
+  for (const key in variables) {
+    if (key.startsWith(values_prefix)) {
+      core.exportVariable(key, variables[key]);
+    }
   }
 } catch ({message}) {
   core.setFailed(message);
